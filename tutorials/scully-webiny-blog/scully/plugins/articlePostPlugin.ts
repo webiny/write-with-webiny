3600f6e3e27e68001641babc3f0e2f4af53fb953
gracegitt <102972030+gracegitt@users.noreply.github.com>
2023-03-21 14:37:19 +0300

    Update plugin to get content by id

--- tutorials/scully-webiny-blog/scully/plugins/articlePostPlugin.ts
+++ tutorials/scully-webiny-blog/scully/plugins/articlePostPlugin.ts
@@ -1,60 +1,56 @@
 import {
-    ApolloClient,
-    ApolloLink,
-    concat,
-    gql,
-    HttpLink,
-    InMemoryCache,
-  } from '@apollo/client/core';
-  import fetch from 'node-fetch';
-  import { HandledRoute, registerPlugin } from '@scullyio/scully';
-  import {environment } from '../../src/environments/environment';
+  ApolloClient,
+  ApolloLink,
+  concat,
+  gql,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client/core';
+import fetch from 'node-fetch';
+import { HandledRoute, registerPlugin } from '@scullyio/scully';
+import { environment } from '../../src/environments/environment';
 
-  export const webinyPostSlugs = 'webinyPostSlugs';
-
-  const authMiddleware = new ApolloLink((operation, forward) => {
-    // add the authorization to the headers
-    operation.setContext({
-      headers: {
-        authorization: `Bearer ${environment.webiny.GRAPHQL_API_TOKEN}`,
-      },
-    });
-    return forward(operation);
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  operation.setContext({
+    headers: {
+      authorization: `Bearer ${environment.webiny.GRAPHQL_API_TOKEN}`,
+    },
   });
-
-  const client = new ApolloClient({
-    link: concat(
-      authMiddleware,
-      new HttpLink({
-        uri: environment.webiny.GRAPHQL_API_URL,
-        fetch,
-      })
-    ),
-    cache: new InMemoryCache(),
-  });
-
-  registerPlugin(
-    'router',
-    'articlePostPlugin',
-
-    async (route: string, config = {}): Promise<HandledRoute[]> => {
-      const {
-        data: { articlePost },
-      } = await client.query({
-        query: gql`
+  return forward(operation);
+});
+const client = new ApolloClient({
+  link: concat(
+    authMiddleware,
+    new HttpLink({
+      uri: environment.webiny.GRAPHQL_API_URL,
+      fetch,
+    })
+  ),
+  cache: new InMemoryCache(),
+});
+registerPlugin(
+  'router',
+  'articlePostPlugin',
+  async (route: string, config = {}): Promise<HandledRoute[]> => {
+    const {
+      data: { listArticlePosts },
+    } = await client.query({
+      query: gql`
         query Data {
-            pageBuilder {
-              listPublishedPages {
-                data {
-                  title
-                }
-              }
+          listArticlePosts {
+            data{
+              id
+              title
+
             }
+          
           }
-        `,
-      });
-      return Promise.resolve(
-        articlePost.map((blog) => ({ route: `/p/${blog.slug}` }))
-      );
-    }
-  );
+        }
+      `,
+    });
+    return Promise.resolve(
+      listArticlePosts.map((post) => ({ route: `blog/${post.id}` }))
+    );
+  }
+);
