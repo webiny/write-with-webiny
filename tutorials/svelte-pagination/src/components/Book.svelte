bd0ab51f4402a0c31fdee1ef75a66b4588f28e03
Taminoturoko Briggs <godsfirstbriggs@gmail.com>
2023-08-25 11:47:32 +0100

    feat(tutorials): add code for svelte pagination tutorial (#89)

---
+++ tutorials/svelte-pagination/src/components/Book.svelte
@@ -0,0 +1,64 @@
+<div class='book'>
+  <img src={image} alt='book'/>
+  <div class='book_desc'>
+    <h4>{title}</h4>
+  {#each authors as author }
+    <div class='book_authors'>
+      <span>{author}</span>
+    </div>
+  {/each}
+    <span>{publisher}, {publishDate}</span>
+  </div>
+</div>
+
+<script>
+  export let title;
+  export let image;
+  export let authors;
+  export let publisher;
+  export let publishDate;
+</script>
+
+<style>
+.book {
+  display: flex;
+  background-color: rgb(243, 241, 246);
+  border-radius: 5px;
+  padding: 5px;
+  width: 100%;
+  max-width: 450px;
+  max-height: 200px;
+}
+
+.book img {
+  margin-right: 10px;
+  width: 30%;
+}
+
+.book_desc{
+  text-align: left;
+}
+
+.book_desc h4 {
+  margin-bottom: 5px;
+  font-size: 18px;
+}
+
+.book_desc > span{
+  color: gray;
+  font-size: 14px;
+}
+
+.book_authors{
+  color: gray;
+  font-size: 14px;
+  margin-bottom: 5px;
+}
+
+@media (min-width: 600px){
+
+  .book img {
+    width: 32%;
+  }
+}
+</style>
